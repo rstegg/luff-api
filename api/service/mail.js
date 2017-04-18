038ccb0f5f52702f1bd8644cfdc4add1953a8338
Ryan <stegartie@gmail.com>
2017-04-17 21:36:35 -0700

    Clean up some of api

--- api/service/mail.js
+++ api/service/mail.js
@@ -2,11 +2,4 @@ const api_key = process.env.MAILGUN_SECRET
 const domain = 'mg.luvpay.io'
 const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain})
 
-// var data = {
-//   from: 'Excited User <me@samples.mailgun.org>',
-//   to: 'serobnic@mail.ru',
-//   subject: 'Hello',
-//   text: 'Testing some Mailgun awesomness!'
-// }
-
 module.exports = mailgun
